155367455df3be559404b5c2f6b6bc30cb0c2a26
ViniCloud <vinicius.gusmao.hxh@outlook.com>
2026-01-14 20:50:57 -0300

    feat:finish modulo 2

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text} from "react-native"
+import {style} from "./style"
+
+export  function Home() {
+ return(
+   <View style={style.container}>
+      <Text style={style.Text}>Aoba porra</Text>
+   </View>
+ )
+}
+
---
+++ src/app/Home/style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+export const style = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems:"center"
+  },
+  Text:{
+    backgroundColor: "green",
+    fontWeight:700,
+    fontSize:34,
+    color:"red",
+    boxShadow: "15px 15px 10px 5px blue"
+  }
+})
