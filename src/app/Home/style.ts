4d81c8a5fb965a127170737be9fd2d4fed0ca20e
ViniTrampo <morangoaranha@gmail.com>
2026-01-27 17:07:40 -0300

    feat: criação de assets e logo do app

--- src/app/Home/style.ts
+++ src/app/Home/style.ts
@@ -1,16 +1,13 @@
 import { StyleSheet } from "react-native"
 
 export const style = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     justifyContent: "center",
-    alignItems:"center"
+    alignItems: "center"
   },
-  Text:{
-    backgroundColor: "green",
-    fontWeight:700,
-    fontSize:34,
-    color:"red",
-    boxShadow: "15px 15px 10px 5px blue"
+  logo: {
+    height: 34,
+    width: 134
   }
 })
